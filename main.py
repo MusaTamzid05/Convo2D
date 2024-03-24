1f36e4e97caa962740d19407a52e53f6d91d86e8
MusaTamzid05 <tamzid101@gmail.com>
2024-03-25 03:48:10 +0600

    Calutate Convo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import random
+import time
 
 def calculate_convo_width(width, kernel_size, stride, padding):
     return ((width - kernel_size + (2 * padding)) /stride) + 1
@@ -8,11 +9,77 @@ class Conv2D:
     This is for educational perpose.The code is not optimize for speed
     but for understanding of mortals(mee!!)
     '''
-    def __init__(self, width):
-        pass
+    def __init__(self, mat, kernel_size=3, stride=1, padding=0):
+        self.mat = mat
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.kernel = []
+
+        for _ in range(kernel_size):
+            row = []
+            for _ in range(kernel_size):
+                row.append(random.uniform(0, 1))
+            self.kernel.append(row)
+
+
+
+
+
+    def forward(self):
+        result = []
+        result_row = []
+
+        for row in range(0, len(self.mat), self.stride):
+            for col in range(0, len(self.mat[0]), self.stride):
+
+                if (row  + (self.kernel_size - 1)) >= len(self.mat):
+                    continue
+
+                if (col + (self.kernel_size - 1)) >= len(self.mat[0]):
+                    if len(result_row):
+                        result.append(result_row)
+                        result_row = []
+
+                    continue
+
+                sum_ = 0
+
+                for i in range(self.kernel_size):
+                    for j in range(self.kernel_size):
+                        sum_ += self.kernel[i][j] * self.mat[row + i][col + j]
+
+                result_row.append(sum_)
+
+        if len(result_row):
+            result.append(result_row)
+
+
+        return result
+
+
+
+
+
+
 
 def main():
+    random.seed(time.time())
     print(calculate_convo_width(width=5, kernel_size=3, stride=2, padding=0))
+
+    mat = [
+            [1,1,1,1,1],
+            [1,1,1,1,1],
+            [1,1,1,1,1],
+            [1,1,1,1,1],
+            [1,1,1,1,1],
+            ]
+
+    convo = Conv2D(mat=mat, stride=2)
+    result = convo.forward()
+
+    for row in result:
+        print(row)
 
 if __name__ == "__main__":
     main()
